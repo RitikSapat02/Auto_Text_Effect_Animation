e8548b88249bd89b0241831e88bd2c9d6642611e
Ritik Sapat <118529770+RitikSapat02@users.noreply.github.com>
2023-05-09 20:31:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('.container');
+
+const careers = ['YouTuber','Web Developer','Freelancer','Instructor',]
+
+let careerIndex = 0;
+let characterIndex = 0;
+
+function updateText(){
+    characterIndex++;
+    container.innerHTML = `<h1>I am ${careers[careerIndex].slice(0,1)==='I'? 'an':'a'} ${careers[careerIndex].slice(0,characterIndex)}</h1>`;
+
+    if(characterIndex === careers[careerIndex].length){
+        careerIndex++;
+        characterIndex = 0;
+    }
+
+    if(careerIndex === careers.length){
+        careerIndex=0;
+    }
+    
+    setTimeout(updateText,200);
+}
+
+updateText();
